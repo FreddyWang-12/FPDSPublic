921919d677eee2dbcf3a28c3ba946dfadcce8c3f
Freddy Wang <fcwang@mail.smu.edu>
2020-04-18 14:10:02 -0500

    add word.h

---
+++ word.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Frederick Wang on 4/18/20.
+//
+
+#include "word.h"
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+word::word(){
+    data = "";
+}
+
+word::word(string newOne){
+    data = newOne;
+}
+
+void word::addDoc(string docID) {
+    docIDs.push_back(docID);
+}
+
+string word::findDoc(string docID) {
+    vector<string>::iterator it = find(docIDs.begin(), docIDs.end(), docID);
+    if(it->data() == docID){
+        return docID;
+    }
+    else{
+        return "Negative";
+    }
+}
---
+++ word.h
@@ -0,0 +1,25 @@
+//
+// Created by Frederick Wang on 4/18/20.
+//
+
+#ifndef UNTITLED1_WORD_H
+#define UNTITLED1_WORD_H
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+class word {
+private:
+    string data;
+    vector<string> docIDs;
+
+public:
+    word();
+    word(string newOne);
+    void addDoc(string docID);
+    string findDoc(string docID);
+};
+
+
+#endif //UNTITLED1_WORD_H
