b6c82a83772a3f4ba0cf878a10e97429c9ae2379
Jonas Moros <jonasemoros@gmail.com>
2020-05-02 13:26:00 -0500

    Progress

---
+++ DocumentOBJ.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jonas on 5/2/2020.
+//
+
+#include "DocumentOBJ.h"
---
+++ DocumentOBJ.h
@@ -0,0 +1,14 @@
+//
+// Created by jonas on 5/2/2020.
+//
+
+#ifndef FINALPROJECT_DOCUMENTOBJ_H
+#define FINALPROJECT_DOCUMENTOBJ_H
+
+
+class DocumentOBJ {
+
+};
+
+
+#endif //FINALPROJECT_DOCUMENTOBJ_H
